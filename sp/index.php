5600b1bd00110b439cddfa07cdbc9cba4cb7e8c5
mbouldo <60163895+mbouldo@users.noreply.github.com>
2021-04-29 21:06:48 -0500

    Create index.php

---
+++ sp/index.php
@@ -0,0 +1,6 @@
+<?php
+
+//file that will be loaded first to test php version and successful install
+
+echo '<h2 style="font-family:courier;text-align:center;padding-top:5rem;">Hello World</h3>';
+?>
