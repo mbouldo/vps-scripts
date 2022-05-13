a1cee6f31d57cc68e2f855a375a7fca684033ec2
mbouldo <60163895+mbouldo@users.noreply.github.com>
2022-05-12 20:46:56 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
 
+sudo apt install docker-ce
+sudo apt-get install docker-compose
+
+
+sudo apt-get install nginx
+
+sudo ufw allow 'Nginx Full'
 
 
 wget https://raw.githubusercontent.com/mbouldo/vps-scripts/master/ghost_configs/docker-compose.yml
@@ -17,3 +30,13 @@ sed -i "s/__DOMAIN__/${domain}/g" docker-compose.yml
 sed -i "s/__PROJECT_NAME__/${project_name}/g" docker-compose.yml
 sed -i "s/__MYSQL_ROOT_PASSWORD__/${mysql_root_password}/g" docker-compose.yml
 sed -i "s/__MYSQL_USER_PASSWORD__/${mysql_user_password}/g" docker-compose.yml
+
+
+https://raw.githubusercontent.com/mbouldo/vps-scripts/master/ghost_configs/nginx_conf
+sed -i "s/__DOMAIN__/${domain}/g" nginx_conf
+mv nginx_conf $domain
+mv $domain /etc/nginx/sites-available/$domain
+ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
+unlink /etc/nginx/sites-enabled/default
+
+systemctl reload nginx
