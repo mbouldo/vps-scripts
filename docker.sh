896a32dc7a6b1e860e3a42d29d2fc6d4c7321560
mbouldo <60163895+mbouldo@users.noreply.github.com>
2022-05-12 22:17:47 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,10 +6,10 @@ read username
 
 sudo apt update -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update -y
-apt-cache policy docker-ce -y
+sudo apt-cache policy docker-ce -y
 
 sudo apt install docker-ce -y
 sudo apt-get install docker-compose -y
@@ -22,7 +22,7 @@ sudo apt-get install nginx -y
 sudo ufw allow 'Nginx Full'
 
 
-wget https://raw.githubusercontent.com/mbouldo/vps-scripts/master/ghost_configs/docker-compose.yml
+sudo wget https://raw.githubusercontent.com/mbouldo/vps-scripts/master/ghost_configs/docker-compose.yml
 echo Enter Your Domain: 
 read domain
 echo Enter Your Project Name: 
@@ -33,24 +33,24 @@ echo Enter your mySQL User Password:
 read mysql_user_password
 
 
-wget https://raw.githubusercontent.com/mbouldo/vps-scripts/master/ghost_configs/nginx_conf
+sudo wget https://raw.githubusercontent.com/mbouldo/vps-scripts/master/ghost_configs/nginx_conf
 sed -i "s/__DOMAIN__/${domain}/g" nginx_conf
-mv nginx_conf $domain
-mv $domain /etc/nginx/sites-available/$domain
-ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
-unlink /etc/nginx/sites-enabled/default
+sudo mv nginx_conf $domain
+sudo mv $domain /etc/nginx/sites-available/$domain
+sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
+sudo unlink /etc/nginx/sites-enabled/default
 
-systemctl reload nginx
+sudo systemctl reload nginx
 
-sed -i "s/__DOMAIN__/${domain}/g" docker-compose.yml
-sed -i "s/__PROJECT_NAME__/${project_name}/g" docker-compose.yml
-sed -i "s/__MYSQL_ROOT_PASSWORD__/${mysql_root_password}/g" docker-compose.yml
-sed -i "s/__MYSQL_USER_PASSWORD__/${mysql_user_password}/g" docker-compose.yml
+sudo sed -i "s/__DOMAIN__/${domain}/g" docker-compose.yml
+sudo sed -i "s/__PROJECT_NAME__/${project_name}/g" docker-compose.yml
+sudo sed -i "s/__MYSQL_ROOT_PASSWORD__/${mysql_root_password}/g" docker-compose.yml
+sudo sed -i "s/__MYSQL_USER_PASSWORD__/${mysql_user_password}/g" docker-compose.yml
 
-mv docker-compose.yml ~/$project_name/
-cd ~/$project_name/
+sudo mv docker-compose.yml ~/$project_name/
+sudo cd ~/$project_name/
 
-docker-compose up -d
-docker ps
+sudo docker-compose up -d
+sudo docker ps
 
 
